6902a26670776b269d8e9ccadc7bc8608d4a5d3f
Femi Ajayi <femiajayi705@gmail.com>
2016-07-13 19:24:17 +0800

    Added in_stock? method to the product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,26 @@
 class Product
-	attr_reader :title
+	attr_reader :title, :price, :stock
 
 	@@products = []
 
 	def initialize options={}
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_products
 	end	
 
 	def self.all
 		@@products
 	end	
+
+	def in_stock?
+		if @stock > 0
+			return true
+		else
+			return false
+		end
+	end
 
 	def self.find_by_title txt
 		@@products.detect {|find| find.title == txt}
